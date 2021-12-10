fc3fd77c346efd7666109a082ac9904af5334a4f
Fran Camaj <fran.camaj@limitmarkets.com>
2021-12-10 16:37:12 +0100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -6,6 +7,19 @@ const port: number = +process.env.port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('bSocial')
+    .setDescription('The bSocial API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  config.security = [{ bearer: [] }];
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();

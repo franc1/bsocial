727644291db49edd094165201d8b7690a38418c4
Fran Camaj <fran.camaj@limitmarkets.com>
2021-12-11 14:06:38 +0100

    Add post.create endpoint

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiForbiddenResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { plainToClass } from 'class-transformer';
+import { Token } from 'src/auth/passport-strategies/token.request';
+import { TokenParam } from 'src/decorators/token.decorator';
+import { ErrorResponse } from 'src/shared/error.response';
+
+import { PostCreateDTO } from './models/dto/post-create.dto';
+import { Post as PostModel } from './models/post.model';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+@ApiTags('post')
+@ApiBadRequestResponse({
+  type: ErrorResponse,
+})
+@ApiUnauthorizedResponse({
+  type: ErrorResponse,
+})
+@ApiForbiddenResponse({
+  type: ErrorResponse,
+})
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  async create(
+    @Body() postDTO: PostCreateDTO,
+    @TokenParam() token: Token,
+  ): Promise<PostModel> {
+    const user = await this.postService.create(postDTO, token);
+
+    return plainToClass(PostModel, user);
+  }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Token } from 'src/auth/passport-strategies/token.request';
+
+import { PostCreateDTO } from './models/dto/post-create.dto';
+import { Post } from './models/post.model';
+import { PostRepository } from './post.repository';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private readonly postRepository: PostRepository) {}
+
+  async create(postDTO: PostCreateDTO, token: Token): Promise<Post> {
+    const post = new Post();
+    post.content = postDTO.content;
+    post.createdBy = token.id;
+
+    return await this.postRepository.save(post);
+  }
+}
